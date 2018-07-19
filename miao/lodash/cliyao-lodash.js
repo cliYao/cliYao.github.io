ef9b25545edb542f09175c80c0e3d86daca5844b
cliYao <568077487@qq.com>
2018-07-19 08:38:36 +0800

    lodash

--- miao/lodash/cliyao-lodash.js
+++ miao/lodash/cliyao-lodash.js
@@ -34,16 +34,60 @@ var cliyao = {
   },
   //Creates a slice of array with n elements dropped from the end.
   dropRight:function(array,n=1){
-      return array.slice(0,array.length-n)
+      return array.length-n<0?[]:array.slice(0,array.length-n)
   },
   //Creates a slice of array excluding elements dropped from the end. Elements are dropped until predicate returns falsey. The predicate is invoked with three arguments: (value, index, array).
   xdropRightWhile:function(){
 
+  },
+  //Fills elements of array with value from start up to, but not including, end.
+  fill:function(array, value, start=0, end=array.length){
+      for(i=start;i<end;i++){
+        array[i]=value
+      }
+      return array
+  },
+
+  //This method is like _.find except that it returns the index of the first element predicate returns truthy for instead of the element itself.
+  xfindIndex:function(array, predicate=_.identity, fromIndex=0){
+
   }
+  //Gets the first element of array.
+  head:function(array){
+      return array.length===0?undefined:array[0]
+  },
+  //Gets the index at which the first occurrence of value is found in array using SameValueZero for equality comparisons. If fromIndex is negative, it's used as the offset from the end of array.
+  indexOf:function(array, value, fromIndex=0){
+      for(i=fromIndex;i<array.length;i++){
+        if(array[i]===value){
+          return i
+        }
+      }
+      return []
+  },
+  //Gets all but the last element of array.
+  initial:function(array){
+      return array.splice(0,array.length-1)
+  },
+  //Creates an array of unique values that are included in all given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+  intersection:function([arrays]){
+      var nums=[arrays].pop()
+      var nums1=[].concat(...arrays)
+      var nums2=[]
+      for(i=0;i<nums.length;i++){
+        if(nums1.indexOf(nums[i])!==-1){
+          nums2.push(nums[i])
+        }
+      }
+      return nums
+  },
 
-
-
-
+  xjoin:function(array, separator=','){
+      var result=""
+      for(i=0;i<array.length;i++){
+          result+=separator+array[i]
+      }
+  },
 
 }
 
